dc094779aea0e4efe9e2f2fffe113d9f419dd659
chenxbo <1328408939@qq.com>
2018-02-09 12:05:30 +0800

    modify the backtest, but not finished

--- backtest.py
+++ backtest.py
@@ -11,7 +11,11 @@ import utils
 from sqlalchemy import create_engine
 
 
-def backtest(start, end, engine_quant, engine_price_vol, benchmark_name='000905.SH'):
+def backtest(start, end, engine_quant, engine_price_vol, 
+             benchmark_name='000905.SH',
+             t = 2,
+             Tc = 0.003
+             ):
     '''
     Get backtest indictors according to the stock weights
     
@@ -26,6 +30,11 @@ def backtest(start, end, engine_quant, engine_price_vol, benchmark_name='000905.
             database engine
         benchmark: 
             benchmark name, default '000905.SH' 中证500
+        t:
+            int, Prediction Period, default 2, can't be changed beacause the weights are calculated 
+                 when the period is 2
+        Tc:
+            transaction cost, double cost
     Return:
         backtest indictors, 
     '''
@@ -33,75 +42,84 @@ def backtest(start, end, engine_quant, engine_price_vol, benchmark_name='000905.
     price = utils.get_close(start, end, engine_quant)
     benchmark_rtn = utils.get_benchmark(start, end, engine_quant, benchmark_name)
     stk_weight = utils.get_weight2(start, end, engine_price_vol)
+    stk_weight = stk_weight.groupby(level='trade_date').apply(truncate_weights)
+    stk_weight = stk_weight[stk_weight != 0]
+
+#   price 和 stk_weight的index要完全匹配
     
-    portfolio_rtn = 
+    # Real Trade Date in the period of backtesting
+    td = sorted(list(
+                      set(price.index.levels[0].tolist())
+                    & set(benchmark_rtn.index.tolist())
+                    & set(stk_weight.index.levels[0].tolist())
+                    ))
+    turnover_td = td[0: : t] # the date series of turnover 
+    
+    holding = {}
+    weight_pro = stk_weight.loc[td[0]]
+    temp = initial_capital * weight_pro / price.loc[td[0]].reindex(weight_pro.index)
+    holding[td[0]] = temp
+
+    for dt in td[1:]:
+        if dt in turnover_td:
+            capital = (price.loc[dt].reindex(temp.index) * temp).sum()
+            # cost 需要修改，先用并集合
+            cost = Tc * np.abs(stk_weight.loc[dt] 
+                             - weight_pro.reindex(stk_weight.loc[dt].index).fillna(0)).sum() / 2
+            weight_pro = stk_weight.loc[dt]
+            temp = (capital - capital * cost) * weight_pro / price.loc[dt].reindex(weight_pro.index)
+        holding[dt] = temp
+    holding = pd.concat(holding,names=['trade_date', 'stock_id'])        
+    
+    '''
+    if price.reindex(holding.index).isnull().values.any():
+        # print ('############  price has error #############')
+        price = price.reindex(holding.index).groupby(level='code').fillna(method='ffill')
+    else:
+        price = price.reindex(holding.index)
+    '''
+    portfolio_rtn = (holding * price).groupby(level='date').sum().pct_change().fillna(0)
     
     stat = get_stat(portfolio_rtn, benchmark_rtn)
     return stat
 
 
-def get_turnover(stk_weight,lags=1):
-    '''
-    根据权重获取资金序列
-    ## 只适用于等权
-    stk_weight: 
-        pd.Series Multi-Index date, asset    weight 换仓期的权重
-    lags: 
-        表示计算当前股票相对于lags期前股票的差异
-    返回换手率时间序列:换手率 = 当期相对于lags期前新加入的股票 / 当期股票总数
-    '''
-    sets = stk_weight.groupby(level=['date']).apply(lambda x: set(x.index.get_level_values('code')))
-    news = (sets - sets.shift(lags)).dropna()
-    turnover = news.apply(len) / sets.apply(len)
-    turnover.fillna(0.5,inplace=True)
-    return turnover    
+def truncate_weights(weights, min_weight=0.001, rescale=True):
+    """
+    Truncates small weight vectors, i.e. sets weights below a treshold to zero.
+    This can be helpful to remove portfolio weights, which are negligibly small.
     
-def get_rtn_s(stk_weight, price, td, fee=0.003):
-    '''
-    根据持仓权重，获取投资组合资金序列
-    先获得各日期的持仓数量（换仓期为(资金-换仓成本)*股票权重/股票价格），非换仓期为上一天的
-    考虑换手率手续费
-    判断持仓的价格是否存在nan值啥的， 不然影响很大
-    params:
-        stk_weight:
-            pd.Series, multiindex: trade_date and stock_id,  value:weight
-        price: 
-            dataframe, index: date  colulmns: code
-        td: 
-            开始回测日期到结束日期的交易日序列
-        fee: 
-            float, 双边
-    return:
-        rtn_s: 组合收益率序列，Series, index: date  
-    '''
-    initial_capital=10000              
+    Parameters
+    ----------
+    weights: pandas.Series
+        Optimal asset weights.
+    min_weight: float, optional
+        All weights, for which the absolute value is smaller
+        than this parameter will be set to zero.
+    rescale: boolean, optional
+        If 'True', rescale weights so that weights.sum() == 1.
+        If 'False', do not rescale.
 
-    price = price.loc[td[0]:td[-1]].stack().copy()
-    price.index.names = ['date','code']
-    turnover_s = get_turnover(stk_weight)
-    
-    holding = {}
-    holding_pro = initial_capital * stk_weight.loc[td[0]] / price.loc[td[0]].reindex(stk_weight.loc[td[0]].index)
-    holding[td[0]] = holding_pro
-    for dt in td[1:]:
-        if dt in stk_weight.index:
-            capital = (price.loc[dt].reindex(holding_pro.index) * holding_pro).sum()
-            holding_pro = (capital - capital * turnover_s.loc[dt] * fee) * \
-                           stk_weight.loc[dt] / price.loc[dt].reindex(stk_weight.loc[dt].index)
-        holding[dt] = holding_pro
-    holding = pd.concat(holding,names=['holding'])        
-    holding.index.names = ['date','code']
-    if price.reindex(holding.index).isnull().values.any():
-        # 比如2016-08-12号买到 300372，不知道为啥数据就没有价格了（退市，但之后其实还是有价格的），所以默认价格数据等于前一天价格
-        # print ('############  price has error #############')
-        price = price.reindex(holding.index).groupby(level='code').fillna(method='ffill')
-    else:
-        price = price.reindex(holding.index)
+    Returns
+    -------
+    adj_weights: pandas.Series
+        Adjusted weights.
+    """
+    if not isinstance(weights, pd.Series):
+        raise ValueError("Weight vector is not a Series")
+
+    adj_weights = weights[:]
+    adj_weights[adj_weights.abs() < min_weight] = 0.0
+
+    if rescale:
+        if not adj_weights.sum():
+            raise ValueError("Cannot rescale weight vector as sum is not finite")
         
-    rtn_s = (holding * price).groupby(level='date').sum().pct_change().fillna(0)
-    return rtn_s    
-    
+        adj_weights /= adj_weights.sum()
 
+    return adj_weights        
+
+    
 def get_stat(rtn, benchmark_rtn, rf=0):
     '''
     获取回测指标，rtn:可以为单列(Series)，也可以为多列（DataFrame）
